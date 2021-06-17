885cc6e22f1138fd515f447cc3aa9fac19c7f97a
John William Davis <jwdavisdeveloper@gmail.com>
2021-06-17 16:09:32 -0700

    Resize Diego pic

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage src='../images/Diego-nica.jpg' alt='My son, Diego, playing in Nicaragua' />
+      <StaticImage src='../images/Diego-Nica.jpg' alt='My son, Diego, playing in Nicaragua' />
     </Layout>
   );
 };
