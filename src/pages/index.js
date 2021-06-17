b8ed1d8ff39f25cb093e324b7d4372763c9f9641
John William Davis <jwdavisdeveloper@gmail.com>
2021-06-17 16:01:36 -0700

    Switch out image with more recent pic of Diego

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage src='../images/santiaguito.png' alt='My son, Santiago, with his scooter' />
+      <StaticImage src='../images/Diego-nica.jpg' alt='My son, Diego, playing in Nicaragua' />
     </Layout>
   );
 };
