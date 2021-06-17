5ac50fb437722007ceadf2f8addb78a549d989e7
John William Davis <jwdavisdeveloper@gmail.com>
2021-06-17 15:49:56 -0700

    Add David Fincher from URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "Cool Gatsby Davis Site",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src='https://www.artofthetitle.com/assets/sm/upload/zp/xu/iy/zw/df.jpg?k=d079ece891'
+        alt='David Fincher, filmmaker'
+      />
     </Layout>
   );
 };
