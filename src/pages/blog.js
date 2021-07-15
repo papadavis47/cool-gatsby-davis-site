d07abf5fca6eddbe36d25aa73231564fe0255e7c
John William Davis <jwdavisdeveloper@gmail.com>
2021-07-15 11:55:50 -0700

    Finish Part 5, adding MDX and using MDXRenderer

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,27 +1,34 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import { graphql } from "gatsby";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle='My Blog Posts'>
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM D YYYY")
+        }
+        id
+        body
       }
     }
   }
 `;
-
 export default BlogPage;
